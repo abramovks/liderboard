0ddb39ad33693f86bc288a5d0e0e8cc5fbd87be3
Konstantin Abramov <abramovks@yandex.ru>
2021-05-18 15:47:34 +0900

    chor: Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do 
-      #post 'path', to: 'controller#method' 
-      resources :keys
-      resources :records
+      get 'create_key', to: 'keys#create'   
+      get 'liderboard', to: 'records#index' 
+      post 'liderboard', to: 'records#create'  
     end
   end
 
